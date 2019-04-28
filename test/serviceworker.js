19c3e529e58cdeb0ba81c8613106607841c2be17
Jonathan Rowley <inbox@jonathanrowley.net>
2019-04-28 16:29:12 +0100

    Update serviceworker.js

--- test/serviceworker.js
+++ test/serviceworker.js
@@ -2,6 +2,10 @@ self.addEventListener('install', function (event) {
   console.log('install');
 });
 
+self.addEventListener('activate', event => {
+  console.log('V1 now ready to handle fetches!');
+});
+
 self.addEventListener('fetch', function(event) {
   event;
   debugger;
